761bd806591818312e96ff502b40f28adcebabbe
kotoha <k022g1001@g.neec.ac.jp>
2023-05-19 14:49:46 +0900

    02_06_確認課題

---
+++ Player.cpp
@@ -0,0 +1,137 @@
+﻿#include "Player.h"
+#include "ImGuiManager.h"
+#include "MathUtility.h"
+#include "assert.h"
+
+Player::~Player() {
+	// 弾の解放
+	for (PlayerBullet* bullet : bullets_) {
+		delete bullet;
+	}
+}
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	// NULLチェック
+	assert(model);
+
+	// 受け渡し
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	// ワールド初期化
+	worldTransform_.Initialize();
+
+	// シングルインスタンスを取得
+	input_ = Input::GetInstance();
+}
+
+void Player::Update() {
+
+	// デスフラグの立った弾を削除
+	bullets_.remove_if([](PlayerBullet* bullet) {
+		if (bullet->IsDead()) {
+			delete bullet;
+			return true;
+		}
+		return false;
+	});
+
+	// 移動ベクトル
+	Vector3 move = {0, 0, 0};
+	// 移動の速さ
+	const float kCharacterSpeed = 0.2f;
+
+	// 上下左右の移動
+	if (input_->PushKey(DIK_LEFT)) {
+		move.x -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move.x += kCharacterSpeed;
+	}
+	if (input_->PushKey(DIK_DOWN)) {
+		move.y -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_UP)) {
+		move.y += kCharacterSpeed;
+	}
+
+	// 旋回(回転)の速さ
+	const float kRotSpeed = 0.02f;
+	// 押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.rotation_.y += kRotSpeed;
+	} else if (input_->PushKey(DIK_D)) {
+		worldTransform_.rotation_.y -= kRotSpeed;
+	}
+
+	// 攻撃処理
+	Attack();
+
+	// 弾の更新
+	for (PlayerBullet* bullet : bullets_) {
+		bullet->Update();
+	}
+
+	// 移動限界座標
+	const float kMoveLimitX = 30.0f;
+	const float kMoveLimitY = 15.0f;
+
+	// 範囲を超えない処理
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+	// 座標移動
+	worldTransform_.translation_.x += move.x;
+	worldTransform_.translation_.y += move.y;
+	worldTransform_.translation_.z += move.z;
+
+	// 行列更新
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	// 行列転送
+	worldTransform_.TransferMatrix();
+
+	// ImGui
+	// ウィンドウのサイズ、座標の指定
+	ImGui::SetNextWindowPos({0, 0});
+	ImGui::SetNextWindowSize({300, 100});
+
+	ImGui::Begin("Debug");
+	// 座標設定
+	float sliderValue3[3] = {
+	    worldTransform_.translation_.x, worldTransform_.translation_.y,
+	    worldTransform_.translation_.z};
+	ImGui::SliderFloat3("Player", sliderValue3, -20.0f, 20.0f);
+	worldTransform_.translation_ = {sliderValue3[0], sliderValue3[1], sliderValue3[2]};
+
+	ImGui::End();
+}
+
+void Player::Draw(ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+
+	// 弾の描画
+	for (PlayerBullet* bullet : bullets_) {
+		bullet->Draw(viewProjection);
+	}
+}
+
+void Player::Attack() {
+	if (input_->TriggerKey(DIK_SPACE)) {
+
+		// 弾の速度
+		const float kBulletSpeed = 1.0f;
+		Vector3 velocity(0, 0, kBulletSpeed);
+
+		// 速度ベクトルを自機の向きに合わせて回転させる
+		velocity = TransformNormal(velocity, worldTransform_.matWorld_);
+
+		// 弾の生成と初期化
+		PlayerBullet* newBullet = new PlayerBullet;
+		newBullet->Initialize(model_, worldTransform_.translation_, velocity);
+
+		// 弾の登録
+		bullets_.push_back(newBullet);
+	}
+}
---
+++ PlayerBullet.cpp
@@ -0,0 +1,39 @@
+﻿#include "PlayerBullet.h"
+#include <cassert>
+
+void PlayerBullet::Initialize(Model* model, const Vector3& pos, const Vector3& velocity) {
+	// NULLポインタチェック
+	assert(model);
+
+	model_ = model;
+
+	velocity_ = velocity;
+
+	// テクスチャ読み込み
+	texturehandle_ = TextureManager::Load("white1x1.png");
+
+	// 初期化
+	world_.Initialize();
+	world_.translation_ = pos;
+}
+
+void PlayerBullet::Update() {
+
+	// 座標を移動させる
+	world_.translation_.x += velocity_.x;
+	world_.translation_.y += velocity_.y;
+	world_.translation_.z += velocity_.z;
+
+	// 更新
+	world_.UpdateMatrix();
+
+	// 時間経過でデス
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
+}
+
+void PlayerBullet::Draw(ViewProjection& view) {
+	// モデルの描画
+	model_->Draw(world_, view, texturehandle_);
+}
