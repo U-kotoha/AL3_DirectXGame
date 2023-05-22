7d6c907377b2a009037429d9589757b74dd92bdc
kotoha <k022g1001@g.neec.ac.jp>
2023-05-22 14:29:15 +0900

    02_07_確認課題

---
+++ Enemy.cpp
@@ -0,0 +1,26 @@
+﻿#include "Enemy.h"
+
+void Enemy::Initialize() {
+
+}
+
+void Enemy::Update() {
+
+
+	switch (phase_) {
+	case Approch:
+	default:
+		worldTransform_.translation_ += 接近フェーズの速度;
+
+		    if (worldTransform_.translation_.z < 0.0f) {
+			phase_ = Leave;
+		}
+		break;
+	case Leave:
+		worldTransform_.translation_ += 離脱フェーズの速度 break;
+	}
+}
+
+void Enemy::Draw(ViewProjection& viewProjection) {
+
+}
---
+++ Enemy.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// 敵
+/// </summary>
+class Enemy {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection& viewProjection);
+
+	enum class Phase {
+		Approch,
+		Leave,
+	};
+
+private:
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	Phase phase_ = Approch;
+};
